31db4f5f150e45cb2695d8cc29f8eee69d4750cd
Vuradisurafu <ruozversa@yandex.ru>
2022-02-20 22:30:38 +0300

    Create header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="wrapper">
+    <the-nav-bar/>
     <main class="main">
     </main>
   </div>
 </template>
 
 <script>
+import TheNavBar from './components/TheNavBar.vue';
+
 export default {
+  components: { TheNavBar },
 
 };
 </script>
--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,7 +1,26 @@
 <template>
   <header class="header">
     <div class="header__content _container">
-
+      <a href="#" class="header__logo">
+        <img src="../assets/Logo.png" alt="Instagram logo">
+      </a>
+      <div class="header__search search">
+      <input class="search__input" type="text" placeholder="Search">
+      </div>
+      <nav class="header__menu menu">
+        <ul class="menu__list">
+          <li class="menu__item"><a href="" class="menu__link _icon-home"></a></li>
+          <li class="menu__item"><a href="" class="menu__link _icon-messenger"></a></li>
+          <li class="menu__item"><a href="" class="menu__link _icon-new-posts"></a></li>
+          <li class="menu__item"><a href="" class="menu__link _icon-navigate"></a></li>
+          <li class="menu__item"><a href="" class="menu__link _icon-favourite"></a></li>
+          <li class="menu__item">
+            <a href="" class="menu__link">
+            <img src="../assets/profile/girl.png" alt="">
+             </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </header>
 </template>
